d996b86958b657469a6ef9be564aaf3e7e550648
Adam O'Reilly <110822205+adam-invicara@users.noreply.github.com>
2024-05-20 16:47:34 +0400

    fix: updating dependency output

--- remote-component.config.js
+++ remote-component.config.js
@@ -4,10 +4,6 @@
 
 module.exports = {
   resolve: {
-    react: require("react"),
-    "@fullcalendar/core": require("@fullcalendar/core"),
-    "@fullcalendar/daygrid": require("@fullcalendar/daygrid"),
-    "@fullcalendar/multimonth": require("@fullcalendar/multimonth"),
-    "@fullcalendar/react": require("@fullcalendar/react")
+    react: require("react")
   }
 };
